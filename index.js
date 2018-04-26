3ce25ee8345e4b58174606a768ae2f5e3fe006d5
manh.luu <manh.luungoc@vietis.co.jp>
2018-04-26 12:48:55 +0900

    add dynamodb & dynamodb local

--- index.js
+++ index.js
@@ -3,9 +3,73 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const AWS = require('aws-sdk');
 
-app.get('/',function (red, res) {
+const USERS_TABLE = process.env.USERS_TABLE;
+
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDb;
+if (IS_OFFLINE === 'true') {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  })
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient();
+};
+
+app.use(bodyParser.json({strict: false}));
+
+app.get('/', function (req, res) {
     res.send('Hello World');
+})
+
+app.get('/users/:userId', function (req, res) {
+    const params = {
+        TableName: USERS_TABLE,
+        Key: {
+            userId: req.params.userId
+        }
+    }
+
+    dynamoDb.get(params, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(400).json({error: 'Could not get user'});
+        }
+        if(result.Item){
+            const {userId, name} = result.Item;
+            res.json({userId, name});
+        }else{
+            res.status(400).json({error: 'User not found'});
+        }
+    })
+})
+
+app.post('/users', function(req, res){
+    const {userId, name} = req.body;
+    if(typeof userId != 'string'){
+        res.status(400).json({error: 'userId must be a string'});
+    }else if (typeof name != 'string'){
+        res.status(400).json({error: 'name must be a string'});
+    }
+
+    const params = {
+        TableName: USERS_TABLE,
+        Item: {
+            userId: userId,
+            name: name
+        }
+    }
+
+    dynamoDb.put(params, (err) => {
+        if(err) {
+            console.log(err);
+            res.status(400).json({err: 'Could not create user'});
+        }
+        res.json({userId, name});
+    })
 })
 
 module.exports.handler = serverless(app);
